f8b69ad42cfbc42c576048517e77494636c7c021
vikornienko <vikornienko76@gmail.com>
2022-11-06 19:07:14 +0500

    Шестой коммит Сохранение новости из формы.

--- appnews/forms.py
+++ appnews/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from .models import Category
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=128)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=128,
+                            label='News title:',
+                            widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label='News content:',
+                              required=False,
+                              widget=forms.Textarea(attrs={"class": "form-control", "rows": 5}))
+    is_published = forms.BooleanField(label='Publish:', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                      label='News category',
+                                      empty_label='Choice category',
+                                      widget=forms.Select(attrs={"class": "form-control"}))
--- appnews/views.py
+++ appnews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
 from .forms import NewsForm
@@ -29,7 +29,10 @@ def get_one_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            News.objects.create(**form.cleaned_data)
+            return redirect('home')
     else:
         form = NewsForm()
     context_page = {'form': form}
